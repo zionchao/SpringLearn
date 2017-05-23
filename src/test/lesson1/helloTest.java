8a4820e445161a0c842b548140ba548d3092003c
zhangchao <zionchao@qq.com>
2017-05-23 17:40:48 +0800

    spring学习 0523

--- src/test/lesson1/helloTest.java
+++ src/test/lesson1/helloTest.java
@@ -10,22 +10,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.lesson1.Helloworld;
 import com.lesson1.IOutputGenerator;
+import com.lesson1.OutputHelper;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class helloTest {
 
 	@Test
 	public void testHelloworld() {
-		ApplicationContext context=new ClassPathXmlApplicationContext("/resource/helloworld.xml");
+		ApplicationContext context=new ClassPathXmlApplicationContext("/resource/lesson1/helloworld.xml");
 		Helloworld helloworld=(Helloworld) context.getBean("helloworld");
 		helloworld.say();
 	}
 	
 	@Test
 	public void testOutput() {
-		ApplicationContext context=new ClassPathXmlApplicationContext("/resource/helloworld.xml");
-		IOutputGenerator output=(IOutputGenerator) context.getBean("OutputHelper");
-		output.generateOutput();
+		ApplicationContext context=new ClassPathXmlApplicationContext("/resource/lesson1/helloworld.xml");
+		OutputHelper output=(OutputHelper) context.getBean("OutputHelper");
+		output.generatorOutput();
 	}
 
 	
